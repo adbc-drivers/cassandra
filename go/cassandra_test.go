6438d0b150d8af96ecedb508e2c74554a8cafe5d
Mandy Alimaa <mandukhaialimaa@gmail.com>
2026-08-20 13:22:34 -0500

    fix

--- go/cassandra_test.go
+++ go/cassandra_test.go
@@ -355,7 +355,7 @@ func (q *CassandraQuirks) GetMetadata(code adbc.InfoCode) any {
 	case adbc.InfoDriverVersion:
 		return "(unknown or development build)"
 	case adbc.InfoDriverArrowVersion:
-		return "(unknown or development build)"
+		return "v18.7.0"
 	case adbc.InfoVendorVersion:
 		return regexp.MustCompile(`5\.0\.[0-9]+`)
 	case adbc.InfoVendorArrowVersion:
